d9490862e92d24a69c9ba2dbdfa38d64b7d78405
ndtrung95 <ndtrung95@csu.fullerton.edu>
2017-09-11 14:36:36 -0700

    test

---
+++ app/gmaps.py
@@ -0,0 +1,24 @@
+import googlemaps
+from datetime import datetime
+
+now = datetime.now()
+
+class GMaps():
+	def __init__(self, src1, src2):
+		self.gmaps = googlemaps.Client(key='AIzaSyAKcJa1FN-jN3GU4U5vv35Yvz-ptSfs9IA')
+		self.li = []
+		self.directions = {}
+		self.directions = self.gmaps.directions(src1, src2, mode='walking', departure_time=now)
+
+	def getDirections(self):
+		if not self.directions:
+			return self.li
+		else:	                                 
+			for steps in self.directions[0]['legs'][0]['steps'][:-1]:
+			    self.li.append((steps['html_instructions']) + ' for ' + steps['duration']['text'] + '(' + steps['distance']['text'] + ')')
+			self.li.append(self.directions[0]['legs'][0]['steps'][-1]['html_instructions'])
+			self.li.append('Total: ' + self.directions[0]['legs'][0]['duration']['text']+ ', ' + self.directions[0]['legs'][0]['distance']['text'])
+			return self.li
+
+	def getDirectionsObj(self):
+		return self.directions
---
+++ app/views.py
@@ -0,0 +1,22 @@
+from flask import request
+from flask import render_template
+from app import app
+from .gmaps import GMaps
+from .forms import WebForm
+
+@app.route('/')
+def test():
+	form = WebForm(request.form)
+	return render_template('index.html', form=form)
+                           	
+@app.route('/', methods=['POST'])
+def test_post():
+	if request.method == 'POST':
+		src1 = request.form['src1']
+		src2 = request.form['src2']
+	gmaps = GMaps(src1, src2)	
+	directions = gmaps.getDirections()
+	if not directions:
+		return render_template('empty.html')
+	else:
+		return render_template('gmaps.html', directions=directions)
