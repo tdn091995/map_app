1ec68b8d2e5c97f5648754d925d21d86b2504bbf
ndtrung95 <ndtrung95@csu.fullerton.edu>
2018-03-16 15:29:30 -0700

    working directions with buttons

--- app/views.py
+++ app/views.py
@@ -6,6 +6,8 @@ from .gmaps import GMaps
 from .building import getBuilding
 from .key import getKey
 
+
+
 @app.route('/')
 def home():
 	key = getKey()
@@ -15,20 +17,22 @@ def home():
 def mapdemo():
 	key = getKey()
 	return render_template('campusmap.html', key=key)
-	
+
 @app.route('/map', methods=['POST'])
 def mapdemo_post():
+
 	if request.method == 'POST':
+
 		src = request.form['src1']
 		cur = request.form['location']
 	key = getKey()
 	info = getBuilding(src)
 	bld = info[0]
-	coords = info[1]	
+	coords = info[1]
 	gmaps = GMaps(cur, coords)
 	directions = gmaps.getDirections()
 	tl = gmaps.getTripLength()
 	if not src:
 		return render_template('campusmap.html')
 	else:
-		return render_template('directions.html', bld=bld, coords=coords, directions=directions, tl=tl, key=key)
+		return render_template('directions.html', bld=bld, coords=coords, cur=cur, directions=directions, tl=tl, key=key)
