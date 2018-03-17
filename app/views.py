0d1d714a15cda4b6154a95fe03c766086a58abf0
ndtrung95 <ndtrung95@csu.fullerton.edu>
2018-03-16 17:35:49 -0700

    fix 400 error

--- app/views.py
+++ app/views.py
@@ -1,12 +1,11 @@
 from flask import request
 from flask import render_template
 from flask import jsonify
+from flask import session
 from app import app
 from .gmaps import GMaps
 from .building import getBuilding
 from .key import getKey
-
-
 
 @app.route('/')
 def home():
@@ -20,18 +19,21 @@ def mapdemo():
 
 @app.route('/map', methods=['POST'])
 def mapdemo_post():
-
 	if request.method == 'POST':
-
 		src = request.form['src1']
-		cur = request.form['location']
+		current = request.form['location']
+		if current != '':
+			session['curLoc'] = current
+		#cur = request.form['location']
 	key = getKey()
+	#info = getBuilding(src)
 	info = getBuilding(src)
 	bld = info[0]
 	coords = info[1]
-	gmaps = GMaps(cur, coords)
+	gmaps = GMaps(session['curLoc'], coords)
 	directions = gmaps.getDirections()
 	tl = gmaps.getTripLength()
+	cur = session['curLoc']
 	if not src:
 		return render_template('campusmap.html')
 	else:
