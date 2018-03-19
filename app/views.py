e11dca02606e1ad2c307719da784e85660e7edb2
Asit Kumar Dash <31290876+asitkdash@users.noreply.github.com>
2018-03-18 18:00:59 -0700

    geomarker and direction method added

--- app/views.py
+++ app/views.py
@@ -1,6 +1,7 @@
 from flask import request
 from flask import render_template
 from flask import jsonify
+from flask import session
 from app import app
 from .gmaps import GMaps
 from .building import getBuilding
@@ -15,20 +16,30 @@ def home():
 def mapdemo():
 	key = getKey()
 	return render_template('campusmap.html', key=key)
-	
+
 @app.route('/map', methods=['POST'])
 def mapdemo_post():
 	if request.method == 'POST':
 		src = request.form['src1']
-		cur = request.form['location']
-	key = getKey()
-	info = getBuilding(src)
-	bld = info[0]
-	coords = info[1]	
-	gmaps = GMaps(cur, coords)
-	directions = gmaps.getDirections()
-	tl = gmaps.getTripLength()
+		current = request.form['location']
+		if current != '':
+			session['curLoc'] = current
 	if not src:
 		return render_template('campusmap.html')
 	else:
-		return render_template('directions.html', bld=bld, coords=coords, directions=directions, tl=tl, key=key)
+		key = getKey()
+		info = getBuilding(src)
+		bld = info[0]
+		coords = info[1]
+		gmaps = GMaps(session['curLoc'], coords)
+		directions = gmaps.getDirections()
+		tl = gmaps.getTripLength()
+		cur = session['curLoc']
+		return render_template('directions.html', bld=bld, coords=coords, cur=cur, directions=directions, tl=tl, key=key)
+
+@app.context_processor
+def coords_processor():
+	def getCoords(src):
+		y = getBuilding(src)
+		return y[1]
+	return dict(getCoords=getCoords)
